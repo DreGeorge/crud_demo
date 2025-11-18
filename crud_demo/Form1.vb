e55901cb539b7a790ac5227d79d35aebb09f7ed6
DreGeorge <AndreiWilliamTaborda@SMU-0101>
2025-11-18 09:50:30 +0800

    Connected to MySQLDatabse

--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -1,3 +1,18 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost; userid=root; password=root; database=crud_demo_db"
 
+        Try
+            conn.Open()
+            MessageBox.Show("Connected")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            conn.Close()
+        End Try
+
+    End Sub
 End Class
