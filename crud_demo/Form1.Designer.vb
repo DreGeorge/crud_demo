275ccb1fdea16fd0743abb7dde067f3bbc54dcc7
DreGeorge <AndreiWilliamTaborda@SMU-0101>
2025-11-21 08:17:06 +0800

    Created REad Button

--- crud_demo/Form1.Designer.vb
+++ crud_demo/Form1.Designer.vb
@@ -22,96 +22,133 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnConnect = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxAge = New System.Windows.Forms.TextBox()
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.ButtonInsert = New System.Windows.Forms.Button()
+        Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'btnConnect
+        'ButtonConnect
         '
-        Me.btnConnect.Location = New System.Drawing.Point(266, 66)
-        Me.btnConnect.Name = "btnConnect"
-        Me.btnConnect.Size = New System.Drawing.Size(189, 104)
-        Me.btnConnect.TabIndex = 0
-        Me.btnConnect.Text = "Connect mySQL"
-        Me.btnConnect.UseVisualStyleBackColor = True
+        Me.ButtonConnect.Location = New System.Drawing.Point(266, 38)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(189, 104)
+        Me.ButtonConnect.TabIndex = 0
+        Me.ButtonConnect.Text = "Connect mySQL"
+        Me.ButtonConnect.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'TextBoxName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(355, 176)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.TextBoxName.Location = New System.Drawing.Point(355, 156)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxName.TabIndex = 1
         '
-        'TextBox2
+        'TextBoxAge
         '
-        Me.TextBox2.Location = New System.Drawing.Point(355, 202)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 2
+        Me.TextBoxAge.Location = New System.Drawing.Point(355, 183)
+        Me.TextBoxAge.Name = "TextBoxAge"
+        Me.TextBoxAge.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxAge.TabIndex = 2
         '
-        'TextBox3
+        'TextBoxEmail
         '
-        Me.TextBox3.Location = New System.Drawing.Point(355, 228)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 3
+        Me.TextBoxEmail.Location = New System.Drawing.Point(355, 209)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxEmail.TabIndex = 3
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(263, 183)
+        Me.Label1.Location = New System.Drawing.Point(263, 159)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
         Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Name"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(263, 209)
+        Me.Label2.Location = New System.Drawing.Point(263, 186)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(26, 13)
         Me.Label2.TabIndex = 5
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "Age"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(263, 235)
+        Me.Label3.Location = New System.Drawing.Point(263, 212)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
         Me.Label3.TabIndex = 6
-        Me.Label3.Text = "Label3"
+        Me.Label3.Text = "Email"
+        '
+        'ButtonInsert
+        '
+        Me.ButtonInsert.Location = New System.Drawing.Point(380, 235)
+        Me.ButtonInsert.Name = "ButtonInsert"
+        Me.ButtonInsert.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonInsert.TabIndex = 7
+        Me.ButtonInsert.Text = "Create"
+        Me.ButtonInsert.UseVisualStyleBackColor = True
+        '
+        'ButtonRead
+        '
+        Me.ButtonRead.Location = New System.Drawing.Point(380, 264)
+        Me.ButtonRead.Name = "ButtonRead"
+        Me.ButtonRead.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonRead.TabIndex = 8
+        Me.ButtonRead.Text = "Read"
+        Me.ButtonRead.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(527, 38)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 9
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ButtonRead)
+        Me.Controls.Add(Me.ButtonInsert)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.TextBoxEmail)
+        Me.Controls.Add(Me.TextBoxAge)
+        Me.Controls.Add(Me.TextBoxName)
+        Me.Controls.Add(Me.ButtonConnect)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents btnConnect As Button
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxAge As TextBox
+    Friend WithEvents TextBoxEmail As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonInsert As Button
+    Friend WithEvents ButtonRead As Button
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -2,9 +2,10 @@
 Public Class Form1
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
-    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
         conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost; userid=root; password=root; database=crud_demo_db"
+        conn.ConnectionString = "server=localhost; userid=root; password=root; database=crud_demo_db;"
 
         Try
             conn.Open()
@@ -12,6 +13,41 @@ Public Class Form1
         Catch ex As Exception
             MessageBox.Show(ex.Message)
             conn.Close()
+        End Try
+    End Sub
+
+    Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
+        Dim query As String = "INSERT INTO student_tbl (name, age, email) VALUES (@name, @age, @email)"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@age", CInt(TextBoxAge.Text))
+                    cmd.Parameters.AddWithValue("@email", TextBoxEmail.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record insert successfully!")
+
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        Dim query As String = "SELECT * FROM crud_demo_db.student_tbl;"
+
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn) 'Get from Database
+                Dim table As New DataTable() 'Table Object
+                adapter.Fill(table) 'From adapter to Table Object
+
+                DataGridView1.DataSource = table 'Display to DataGridView
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
         End Try
 
     End Sub
