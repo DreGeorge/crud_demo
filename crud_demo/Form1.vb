d15ed58381a2fdcf269be182f09bcb7c520d4088
DreGeorge <hed-ataborda@smu.edu.ph>
2025-12-02 08:18:51 +0800

    update-delete

--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -87,47 +87,55 @@ Public Class Form1
     End Sub
 
     Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
-        Dim query As String = "UPDATE 'crud_demo_db'.'student_tbl' SET 'name' = @name, 'age' = @age, 'email' = @email WHERE ('id' = @id);"
+        Dim query As String = "UPDATE student_tbl SET name = @name, age = @age, email = @email WHERE id = @id"
 
         Try
             Using conn As New MySqlConnection(connectionString)
                 conn.Open()
                 Using cmd As New MySqlCommand(query, conn)
-                    cmd.Parameters.AddWithValue("id", CInt(TextBoxHiddenId.Text))
-                    cmd.Parameters.AddWithValue("name", TextBoxName.Text)
-                    cmd.Parameters.AddWithValue("age", CInt(TextBoxAge.Text))
-                    cmd.Parameters.AddWithValue("email", TextBoxEmail.Text)
+
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxHiddenId.Text))
+                    cmd.Parameters.AddWithValue("@name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@age", CInt(TextBoxAge.Text))
+                    cmd.Parameters.AddWithValue("@email", TextBoxEmail.Text)
+
                     cmd.ExecuteNonQuery()
                     MessageBox.Show("Record updated successfully.")
-
                 End Using
-
-
             End Using
+
+            LoadTable()
+
         Catch ex As Exception
             MsgBox(ex.Message)
-
         End Try
-
     End Sub
 
     Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
-        Dim query As String = "DELETE FROM 'crud_demo_db'.'student_tbl' WHERE ('id' = @id);"
+        Dim query As String = "DELETE FROM student_tbl WHERE id = @id"
 
         Try
             Using conn As New MySqlConnection(connectionString)
                 conn.Open()
                 Using cmd As New MySqlCommand(query, conn)
-                    cmd.Parameters.AddWithValue("id", CInt(TextBoxHiddenId.Text))
+
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxHiddenId.Text))
                     cmd.ExecuteNonQuery()
+
                     MessageBox.Show("Record deleted successfully")
+
                     TextBoxName.Clear()
                     TextBoxAge.Clear()
                     TextBoxEmail.Clear()
+                    TextBoxHiddenId.Clear()
                 End Using
             End Using
-        Catch ex As Exception
 
+            LoadTable()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
         End Try
     End Sub
+
 End Class
